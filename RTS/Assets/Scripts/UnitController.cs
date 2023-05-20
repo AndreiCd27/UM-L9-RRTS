3b02b982b658b5f67fa1aeb963a30500fa0d1e64
AndreiCd27 <andreic271113@gmail.com>
2023-05-20 17:41:29 +0300

    Stage 3, animations

--- RTS/Assets/Scripts/UnitController.cs
+++ RTS/Assets/Scripts/UnitController.cs
@@ -5,6 +5,11 @@ using UnityEngine.AI;
 
 public class UnitController : MonoBehaviour
 {
+    public static float delay = 0.5f;
+    private float health;
+    private float damage;
+    public float Health { get { return health; } set { health = value; } }
+    public float Damage { get { return damage; } set { damage = value; } }
     private NavMeshAgent agent;
     private Animator anim;
     private RaycastHit rayHit;
@@ -15,7 +20,8 @@ public class UnitController : MonoBehaviour
         agent = GetComponent<NavMeshAgent>();
         anim = GetComponent<Animator>();
         mySelectable = GetComponent<MySelectable>();
-
+        Health = 500;
+        Damage = 100;
     }
 
     // Update is called once per frame
@@ -23,17 +29,65 @@ public class UnitController : MonoBehaviour
     {
         Move();
     }
-
-    void Move() {
+    void Move()
+    {
+        // foreach (var item in MySelectable.allMySelectables)
+        // {
+        //     if (mySelectable.selected && Input.GetMouseButtonDown(0))
+        //     {
+        //         anim.SetFloat("Running", agent.remainingDistance);
+        //         if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out rayHit, 100))
+        //         {
+        //             agent.destination = rayHit.point;
+        //         }
+        //     }
+        //     else{
+        //         anim.SetFloat("Running", 0f);
+        //     }
+        // }
         foreach (var item in MySelectable.allMySelectables)
         {
-            if (mySelectable.selected && Input.GetMouseButtonDown(0))
+            // pentru a putea face animatia trebuie ca mai intai sa fie selectat
+            if (mySelectable.selected)
             {
-                if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition),out rayHit,100))
+                anim.SetFloat("Running", agent.remainingDistance);
+                // dupa ce apasam click distanta se modifica si animatia incepe
+                if (Input.GetKeyDown("space"))
                 {
-                    agent.destination = rayHit.point;
+                    if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out rayHit, 100))
+                    {
+                        agent.destination = rayHit.point;
+                    }
                 }
             }
+            else
+                anim.SetFloat("Running", 0f);
         }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            agent.destination = other.transform.position;
+            // agent.destination = other.gameObject.transform.position;
+            anim.SetBool("Attack", true);
+            StartCoroutine(Attack(other));
+        }
+    }
+    IEnumerator Attack(Collider col)
+    {
+        while (col != null && col.GetComponent<EnemyController>().Health > 0)
+        {
+            transform.LookAt(col.transform);
+            col.GetComponent<EnemyController>().Health -= Damage;
+            yield return new WaitForSeconds(delay * 3f);
+        }
+        anim.SetBool("Attack", false);
+        if (col != null)
+        {
+            Destroy(col.gameObject, delay);
+        }
+
+
     }
 }
